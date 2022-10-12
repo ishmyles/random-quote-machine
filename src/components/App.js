05f5137516f05df26ac24e6f07a84573806b97fd
Myles Gaerlan <ish.myles@gmail.com>
2022-10-12 19:45:56 +1100

    Added fuctionality to get/share random quotes

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,54 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div id="quote-box">
-        <figure id="quote">
-          <blockquote className='blockquote'>
-            <span className='quote left-quote text-lg'>❝</span>
-            <p id="text"><strong>Lorem ipsum something blah blah</strong></p>
-            <span className='quote right-quote text-lg'>❞</span></blockquote>
-          <figcaption class="blockquote-footer text-end">
-            <p id="author">Author name</p>
-          </figcaption>
-        </figure>
-        <div id="btns">
-          <a href="http://twitter.com/intent/tweet" id="tweet-quote" class="btn btn-dark"><i class="fab fa-twitter"></i></a>
-          <button type='button' class="btn btn-dark" id="new-quote">New Quote</button>
-        </div>
-      </div>
-      <img src='/images/naruto-img-sm.png' alt="Naruto looking up" id="img-hero" class="img"/>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      quote: "",
+      author: ""
+    }
+
+    this.getAnimeQuote = this.getAnimeQuote.bind(this);
+  }
+
+  async getAnimeQuote() {
+    fetch('https://animechan.vercel.app/api/random')
+      .then(response => response.json())
+      .then(animeQuote => this.setState({
+        quote: animeQuote.quote,
+        author: animeQuote.character
+      }))
+  }
+  
+  async componentDidMount() {
+    this.getAnimeQuote();
+  }
+
+  render() { 
+    return ( 
+      <div className="container-fluid">
+        <div id="quote-box">
+          <figure id="quote">
+            <blockquote className='blockquote'>
+              <span className='quote left-quote text-lg'>❝</span>
+              <p id="text" style={ (this.state.quote.length > 165) ? {fontSize: "0.7em"} : {fontSize: "1.2em"} }><strong>{this.state.quote}</strong></p>
+              <span className='quote right-quote text-lg'>❞</span></blockquote>
+            <figcaption className="blockquote-footer text-end">
+              <p id="author">{this.state.author}</p>
+            </figcaption>
+          </figure>
+          <div id="btns">
+            <a href={"http://twitter.com/intent/tweet?text=" + encodeURIComponent(this.state.quote + " - " + this.state.author)} id="tweet-quote" className="btn btn-dark"><i className="fab fa-twitter"></i></a>
+            <button type='button' onClick={this.getAnimeQuote} className="btn btn-dark" id="new-quote">New Quote</button>
+          </div>
+        </div>
+        <img src='/images/naruto-img-sm.png' alt="Naruto looking up" id="img-hero" className="img"/>
+        <p className='attribution'>Image by <a href="https://pixabay.com/users/hiteshhtsharma-7440487/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5150379">Hitesh Sharma</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5150379">Pixabay</a></p> 
+      </div>
+    );
+  }
+}
+ 
 export default App;
